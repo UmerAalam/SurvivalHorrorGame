15f9b247cd156f08d7682764ede91ce37ba7fe65
UmerAalam <umerrazzaq.dev@gmail.com>
2022-08-26 18:00:53 +0500

    new

--- Scripts/FlameLight.cs
+++ Scripts/FlameLight.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class FlameLight : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private int lightMode;
+    [SerializeField] GameObject flameLight;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (lightMode == 0)
+            StartCoroutine(AnimateLight());
+    }
+    IEnumerator AnimateLight()
+    {
+        lightMode = Random.Range(1,4);
+        if (lightMode == 1)
+            flameLight.GetComponent<Animation>().Play("TorchAnimation1");
+        if (lightMode == 2)
+            flameLight.GetComponent<Animation>().Play("TorchAnimation2");
+        if (lightMode == 3)
+            flameLight.GetComponent<Animation>().Play("TorchAnimation3");
+        yield return new WaitForSeconds(0.99f);
+        lightMode = 0;
     }
 }
