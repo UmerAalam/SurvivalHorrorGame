ccc2284b38640ea26c39180e0ffeee37a8dc0c4e
UmerAalam <umerrazzaq.dev@gmail.com>
2022-08-31 10:52:36 +0500

    new

--- Scripts/Sequences/BFirstTrigger.cs
+++ Scripts/Sequences/BFirstTrigger.cs
@@ -10,18 +10,19 @@ public class BFirstTrigger : MonoBehaviour
     [SerializeField] GameObject textBox;
     [SerializeField] GameObject guideArrow;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        thePlayer.GetComponent<FirstPersonController>().enabled = false;
-        StartCoroutine(ScenePlayer());
-    }
     IEnumerator ScenePlayer()
     {
+        Debug.Log("I called ScenePlayer Function!");
         textBox.GetComponent<Text>().text = "Look like a weapon on table";
         yield return new WaitForSeconds(2.5f);
         textBox.GetComponent<Text>().text = "";
         thePlayer.GetComponent<FirstPersonController>().enabled = true;
         guideArrow.SetActive(true);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+      thePlayer.GetComponent<FirstPersonController>().enabled = false;
+      StartCoroutine(ScenePlayer());
     }
 
 }
