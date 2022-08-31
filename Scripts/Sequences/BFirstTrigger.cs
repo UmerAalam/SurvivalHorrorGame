b9e7bf70920c5b1987ba0936ae717eb9b2075204
UmerAalam <umerrazzaq.dev@gmail.com>
2022-08-31 10:22:42 +0500

    new

--- Scripts/Sequences/BFirstTrigger.cs
+++ Scripts/Sequences/BFirstTrigger.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class BFirstTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject thePlayer;
+    [SerializeField] GameObject textBox;
+    [SerializeField] GameObject guideArrow;
+
+    void OnTriggerEnter()
     {
-        
+      thePlayer.GetComponent<FirstPersonController>().enabled = false;
+      StartCoroutine(ScenePlayer());
+    }
+    IEnumerator ScenePlayer()
+    {
+        textBox.GetComponent<Text>().text = "Look like a weapon on table";
+        yield return new WaitForSeconds(2.5f);
+        textBox.GetComponent<Text>().text = "";
+        thePlayer.GetComponent<FirstPersonController>().enabled = true;
+        guideArrow.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
