46ddee5a98e680a88f89b9ed8c8d6c18faabfc6f
UmerAalam <umerrazzaq.dev@gmail.com>
2022-09-02 16:01:20 +0500

    new

---
+++ Scripts/Sequences/BZJumpTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BZJumpTrigger : MonoBehaviour
+{
+    [SerializeField] AudioSource doorBang;
+    [SerializeField] AudioSource doorMusic;
+    [SerializeField] GameObject theDoor;
+    [SerializeField] GameObject theZombie;
+
+    void OnTriggerEnter(Collider other)
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        theDoor.GetComponent<Animation>().Play("ZombieDoorOpen");
+    }
+}
