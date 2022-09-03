a27d6a1588fca20f7320b15b9a1c439d690896bd
UmerAalam <umerrazzaq.dev@gmail.com>
2022-09-03 08:51:16 +0500

    Update FirePistol.cs

--- Scripts/FirePistol.cs
+++ Scripts/FirePistol.cs
@@ -6,7 +6,9 @@ public class FirePistol : MonoBehaviour
 {
     [SerializeField] GameObject theGun;
     [SerializeField] GameObject muzzleFlash;
-    [SerializeField] AudioSource shoot;
+    [SerializeField] AudioSource shootSound;
+    [SerializeField] int damageAmount = 5;
+    float targetDistance;
     bool isFiring = false;
     void Update()
     {
@@ -20,11 +22,17 @@ public class FirePistol : MonoBehaviour
     }
     IEnumerator FiringPistol()
     {
+        RaycastHit shoot;
+        if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out shoot))
+        {
+            targetDistance = shoot.distance;
+            targetDistance.
+        }
         isFiring = true;
         theGun.GetComponent<Animation>().Play("Shoot");
         muzzleFlash.SetActive(true);
         muzzleFlash.GetComponent<Animation>().Play("MuzzleFlash");
-        shoot.Play();
+        shootSound.Play();
         yield return new WaitForSeconds(0.5f);
         isFiring = false;
     }
