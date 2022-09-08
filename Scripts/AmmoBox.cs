f7109e25227bc522946b7426a0fdf781e5633d0e
UmerAalam <umerrazzaq.dev@gmail.com>
2022-09-08 11:34:18 +0500

    new

--- Scripts/AmmoBox.cs
+++ Scripts/AmmoBox.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class AmmoBox : MonoBehaviour
 {
     [SerializeField] GameObject ammoBox;
+    [SerializeField] GameObject ammoDisplayPanel;
 
     private void OnTriggerEnter(Collider other)
     {
+        GlobalAmmo.ammoCount += 7;
         ammoBox.SetActive(false);
+        ammoDisplayPanel.SetActive(true);
     }
 }
--- Scripts/GlobalAmmo.cs
+++ Scripts/GlobalAmmo.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GlobalAmmo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject ammoDisplay;
+    public static int ammoCount;
+    int internalAmmo;
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        internalAmmo = ammoCount;
+        ammoDisplay.GetComponent<Text>().text = "" + ammoCount;
     }
 }
