2821e4aaba10e1ff64d382bfca10c7bbc495ca0e
UmerAalam <umerrazzaq.dev@gmail.com>
2022-09-25 19:28:56 +0500

    Commit

---
+++ Scripts/EyeBall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeBall : MonoBehaviour
+{
+    Vector3 mousePosition;
+
+    private void Update()
+    {
+        mousePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        transform.LookAt(mousePosition);
+    }
+}
