56343a64946422c7ac6d77bfc52268cbe313e350
UmerAalam <umerrazzaq.dev@gmail.com>
2022-09-26 14:32:51 +0500

    new

---
+++ Scripts/SceneController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        StartCoroutine(SplashScreen());
+    }
+    public IEnumerator SplashScreen()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("Main_Menu");
+    }
+}
