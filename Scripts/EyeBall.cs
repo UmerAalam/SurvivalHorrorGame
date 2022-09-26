ad15fd637f8f3e903757aaa594cf5964d24aa75a
UmerAalam <umerrazzaq.dev@gmail.com>
2022-09-26 14:15:01 +0500

    new

--- Scripts/EyeBall.cs
+++ Scripts/EyeBall.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class EyeBall : MonoBehaviour
 {
     Vector3 mousePosition;
-
     private void Update()
     {
         mousePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
